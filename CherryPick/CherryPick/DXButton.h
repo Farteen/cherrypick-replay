47b3e23c5a10929a2fba744047fc3b88fc7647e2
MiracleDebris <miracledebris@gmail.com>
2017-11-21 15:21:46 +0800

    修改

---
+++ CherryPick/CherryPick/DXButton.h
@@ -0,0 +1,13 @@
+//
+//  DXButton.h
+//  CherryPick
+//
+//  Created by XIAO DUAN on 2017/11/21.
+//  Copyright © 2017年 Glasses. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DXButton : UIButton
+
+@end
---
+++ CherryPick/CherryPick/DXButton.m
@@ -0,0 +1,21 @@
+//
+//  DXButton.m
+//  CherryPick
+//
+//  Created by XIAO DUAN on 2017/11/21.
+//  Copyright © 2017年 Glasses. All rights reserved.
+//
+
+#import "DXButton.h"
+
+@implementation DXButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
