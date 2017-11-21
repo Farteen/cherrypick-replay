29801f71bae98e1ced1de06e4297c88d653bb970
farteen <xujavacn@gmail.com>
2017-11-21 15:27:20 +0800

    add ftobject

---
+++ CherryPick/CherryPick/FTObject.h
@@ -0,0 +1,13 @@
+//
+//  FTObject.h
+//  CherryPick
+//
+//  Created by Glasses on 21/11/2017.
+//  Copyright © 2017 Glasses. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTObject : NSObject
+
+@end
---
+++ CherryPick/CherryPick/FTObject.m
@@ -0,0 +1,14 @@
+
+//
+//  FTObject.m
+//  CherryPick
+//
+//  Created by Glasses on 21/11/2017.
+//  Copyright © 2017 Glasses. All rights reserved.
+//
+
+#import "FTObject.h"
+
+@implementation FTObject
+
+@end
